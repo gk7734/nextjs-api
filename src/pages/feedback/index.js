d9caae3f05a6fd4788fe4108fb09b50ea862e1bc
gk7734 <gamekiller7734@naver.com>
2023-08-11 23:07:44 +0900

    동적 API Route 구현

--- src/pages/feedback/index.js
+++ src/pages/feedback/index.js
@@ -1,13 +1,32 @@
+import {Fragment, useState} from "react";
 import {buildFeedbackPath, extractFeedback} from "@/pages/api/feedback";
 
-
 function FeedbackPage(pros) {
+    const [feedbackData, setFeedbackData] = useState();
+    
+    function loadFeedbackHandler(id) {
+        fetch(`/api/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setFeedbackData(data.feedback)
+            })
+    }
+
+
     return (
-        <ul>
-            {pros.feedbackItems.map((item) => (
-                <li key={item.id}>{item.text}</li>
-            ))}
-        </ul>
+        <Fragment>
+            {feedbackData && <p>{feedbackData.email}</p>}
+            <ul>
+                {pros.feedbackItems.map((item) => (
+                    <li key={item.id} className='m-3'>
+                        {item.text}
+                        <button onClick={loadFeedbackHandler.bind(null, item.id)} className="m-1 p-1 bg-blue-600 shadow-2xl shadow-orange-300 rounded-xl hover:bg-blue-400">
+                            Show Details
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </Fragment>
     )
 }
 
